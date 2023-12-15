32a0f01345a5e9b260c7d6636aab539e7292f225
Carlos beltran <137569880+DodLitros@users.noreply.github.com>
2023-12-15 18:59:19 +0000

    Lastimosamente, la carpeta src, los estilos no están cogienfo como deberían, pero en la carpeta randomCardGenerator, está el código que usé en visualStudio, y que sí aplica los estilos.

--- src/app.js
+++ src/app.js
@@ -1,11 +1,46 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+const valores = [
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "J",
+  "Q",
+  "K",
+  "A"
+];
+const pintas = ["♦", "♥", "♠", "♣"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+const azar = array => {
+  return array[Math.floor(Math.random() * array.length)];
 };
+
+const valorhtml = document.getElementById("numero");
+const pintahtml = document.getElementsByClassName("pinta");
+const boton = document.querySelector("button");
+
+const randomCard = () => {
+  const valor = azar(valores);
+  const pinta = azar(pintas);
+
+  for (var i = 0; i < pintahtml.length; i++) {
+    pintahtml[i].textContent = pinta;
+
+    if (pinta === "♥" || pinta === "♦") {
+      pintahtml[i].style.color = "red";
+      valorhtml.style.color = "red";
+    } else {
+      pintahtml[i].style.color = "black";
+      valorhtml.style.color = "black";
+    }
+  }
+
+  valorhtml.innerText = valor;
+};
+
+boton.addEventListener("click", randomCard);
+randomCard();
